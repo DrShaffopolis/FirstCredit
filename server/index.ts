071627e5ea10e324a530200f924736025fb86e74
Chris Shaffer <chris@climbcredit.com>
2017-07-26 16:30:46 -0400

    converting server to TypeScript

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
-const express = require('express');
-const path = require('path');
+import * as express from 'express';
+const session = require('express-session');
+import * as path from 'path';
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.get('/api/', function (req, res) {
 if (app.get('env') !== 'development') {
   app.use(express.static(path.join(__dirname, "/../client/dist")));
 }
+
 
 app.listen(3000);
