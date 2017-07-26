f548c180f992445d7cbf211128a2437db53e0139
Chris Shaffer <chris@climbcredit.com>
2017-07-26 15:00:27 -0400

    got a server rolling

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
-console.log('hi')
+const express = require('express');
+const path = require('path');
 
-console.log('h2')
+const app = express();
+
+app.get('/api/', function (req, res) {
+  res.send('Hello World')
+});
+app.use(express.static(path.join(__dirname, "/../client/dist")));
+
+app.listen(3000);
